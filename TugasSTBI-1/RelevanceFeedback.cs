1f788799d6278da954bb9c319c7cffd35c246d7a
yollalala <yollandasekarrini@gmail.com>
2015-11-20 17:39:45 +0700

    add class RelevanceFeedback

---
+++ TugasSTBI-1/RelevanceFeedback.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasSTBI_1
+{
+    class RelevanceFeedback
+    {
+        public static double calculateNewQuery(string relevanceFeedbackMethod, double wQueryOld,
+                                                    List<double> relevant, List<double> irrelevant)
+        {
+            double result = 0;
+            switch (relevanceFeedbackMethod)
+            {
+                case "rochio":
+                    result = rochio(wQueryOld, relevant, irrelevant);
+                    break;
+                case "regular":
+                    result = ideReg(wQueryOld, relevant, irrelevant);
+                    break;
+                case "dechi":
+                    result = ideDecHi(wQueryOld, relevant, irrelevant[0]);
+                    break;
+                case "pseudo":
+                    break;
+            }
+
+            return result;
+        }
+
+        public static double rochio(double wQueryOld, List<double> relevant, List<double> irrelevant)
+        {
+            double result;
+            result = wQueryOld + (relevant.Sum() / relevant.Count) - (irrelevant.Sum() / irrelevant.Count);
+
+            return result;
+        }
+
+        public static double ideReg(double wQueryOld, List<double> relevant, List<double> irrelevant)
+        {
+            double result;
+            result = wQueryOld + relevant.Sum() - irrelevant.Sum();
+
+            return result;
+        }
+
+        public static double ideDecHi(double wQueryOld, List<double> relevant, double irrelevant)
+        {
+            double result;
+            result = wQueryOld + relevant.Sum() - irrelevant;
+
+            return result;
+        }
+    }
+}
