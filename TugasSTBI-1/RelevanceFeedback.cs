c1eb62bf46cd8b0a05c8bd48dd54b492e95e4f02
yollalala <yollandasekarrini@gmail.com>
2015-12-04 07:42:25 +0700

    remove remove query old 0

--- TugasSTBI-1/RelevanceFeedback.cs
+++ TugasSTBI-1/RelevanceFeedback.cs
@@ -232,15 +232,15 @@ namespace TugasSTBI_1
                 iter++;
             }
 
-            int iter1 = 0;
-            foreach (var item in Program.lQueryWeightOld)
-            {
-                foreach (var subitem in item.Where(v => v.weight <= 0).ToList())
-                {
-                    Program.lQueryWeightOld[iter1].Remove(subitem);
-                }
-                iter++;
-            }
+            //int iter1 = 0;
+            //foreach (var item in Program.lQueryWeightOld)
+            //{
+            //    foreach (var subitem in item.Where(v => v.weight <= 0).ToList())
+            //    {
+            //        Program.lQueryWeightOld[iter1].Remove(subitem);
+            //    }
+            //    iter++;
+            //}
         }
 
         private static void distinctQueryNew()
